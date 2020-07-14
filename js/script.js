f90a489e21a259708bb64b85909a65307e412d87
Koushik Karmakar <koushik.karmakar@tum.de>
2020-07-14 03:12:20 +0200

    revert the fuck up

--- js/script.js
+++ js/script.js
@@ -1,26 +1,3 @@
-jQuery.extend(jQuery.easing,{
-    easeInOutExpo: function(x, t, b, c, d){
-        if (t==0) return b;
-        if (t==d) return b+c;
-        if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
-        return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
-    }
-});
-
-$(document).ready(function(){
-
-    $('a[href=#]').click(function(e){
-        e.preventDefault();
-        $('nav').removeClass('visible');
-        $('html,body').stop().animate({scrollTop: $('.'+$(this).data('scrollto')).offset().top-65 }, 700, 'easeInOutExpo', function(){});
-    });
-
-    $('.toggle-menu').click(function(){
-        $('nav').toggleClass('visible');
-    });
-
-});
-
 
 var TxtRotate = function(el, toRotate, period) {
     this.toRotate = toRotate;
